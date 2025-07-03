a142fa439486b94c8d2683ecbfc0a747eba0b88d
thejerichhss <finchjerich4@gmail.com>
2025-07-03 19:08:01 -0400

    Update DEPENDENCIES.sh

--- DEPENDENCIES.sh
+++ DEPENDENCIES.sh
@@ -1,9 +1,24 @@
-sudo apt install curl
+sudo apt update -y
 
-sudo apt install jq 
+sudo apt install curl -y
 
-sudo apt install nano
+sudo apt install jq -y
 
-sudo apt install espeak
+sudo apt install nano -y
 
-sudo apt install cat
+sudo apt install espeak -y
+
+sudo apt install cat -y
+
+sudo apt install ffmpeg -y
+
+sudo apt install pulseaudio -y
+
+sudo apt install python3 python3-pip -y
+
+pip install openai-whisper --break-system-packages
+
+
+
+
+
